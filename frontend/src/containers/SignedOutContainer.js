e9b314582902a8f4598c33e49635c9d9bcc910c8
kainan54 <kaindoneson@gmail.com>
2020-10-17 14:35:20 -0400

    maps maps maps

--- frontend/src/containers/SignedOutContainer.js
+++ frontend/src/containers/SignedOutContainer.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import FullMap from './FullMap';
 import Login from './Login'
 
 const SignedOutContainer = (props) => {
     return (
         <>
             <Login loginHandler={props.loginHandler} />
-         
+            <FullMap />
         </>
     );
 };
