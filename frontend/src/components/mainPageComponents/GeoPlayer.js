080b75f5914461dfcf3bea49c9b6c6a278e28f4b
kainan54 <kaindoneson@gmail.com>
2020-10-19 12:42:30 -0400

    preppingMerge

--- frontend/src/components/mainPageComponents/GeoPlayer.js
+++ frontend/src/components/mainPageComponents/GeoPlayer.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import SpotifyPlayer from 'react-spotify-web-playback';
-import { User } from 'react-spotify-api'
-import { PlaylistTracks } from 'react-spotify-api'
+import { User, Playlist, PlaylistTracks, Artist } from 'react-spotify-api';
 import { SpotifyApiContext } from 'react-spotify-api';
+import { List, Segment } from 'semantic-ui-react';
+import { ar } from 'date-fns/locale';
 
 
 const GeoPlayer = (props) => {
@@ -29,15 +30,55 @@ const GeoPlayer = (props) => {
                 </User>
                 <SpotifyApiContext.Provider value={token}> 
                 <PlaylistTracks id={props.playlist.split(':')[2]}>
-                    {(tracks, loading, error) => (
-                        tracks.data ? (
-                            tracks.data.items.map(track => (
-                                    <h1 key={track.track.id}>{track.track.name}</h1>
+                {
+                    (tracks) => {
+                        if (tracks.data) {
+                           let mappedTracks = tracks.data.items.map(track => (
+                               <List.Content>
+                                    <List.Header key={track.track.id}>
+                                        {track.track.name}
+                                    </List.Header>
+                                    {console.log(track.track.artists[0].id)}
+                                    <Artist id={track.track.artists[0].id}>
+                                        {
+                                            artist => {
+                                                return artist.data ?   ` by ${artist.data.name}` : null
+                                            }
+                                        }
+
+                                    </Artist>
+                                </List.Content>
                             ))
-                        ) 
-                        : 
-                        null
-                    )}
+
+
+
+                            return (
+                                <Segment inverted>
+                                    
+                                    <Playlist id={props.playlist.split(':')[2]}>
+                                        {
+                                            playlist => {
+                                             return playlist.data ? <h4>Playlist Name: {playlist.data.name}</h4> : null
+                                            }
+                                        }
+                                    </Playlist>
+
+                                    Songs:
+                                    <List divided inverted relaxed>
+                                        <List.Item>
+                                                {mappedTracks}
+                                        </List.Item>
+                                    </List>
+                                </Segment>
+                            )
+                    
+                        } else {
+                            return <h2>Loading</h2>
+                        }
+                    }
+                }
+                   
+                    
                         </PlaylistTracks>
                         </SpotifyApiContext.Provider>
 
