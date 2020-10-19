9e2b1cb44676f4b8f59b74f8ee81a974ab6d3e01
kainan54 <kaindoneson@gmail.com>
2020-10-19 13:37:39 -0400

    good 2 pull

--- frontend/src/containers/maps/ViewMap.js
+++ frontend/src/containers/maps/ViewMap.js
@@ -151,7 +151,7 @@ if (!isLoaded) return 'Loading Maps';
                 onClick={()=>{
                     setSelected(marker);
                     console.log('s',marker)
-                    //props.setPlayer(marker.playlist)
+                    props.setPlayer(marker.playlist)
                     }}
                   />
                 ))}
